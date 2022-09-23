c536cb0b7dba37c7c72b2b3a9a17dddbbf632fcf
Adam Thomas <thomasadam83@hotmail.com>
2022-09-23 09:35:03 -0400

    Added sample navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter, Switch, Link} from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Navigation Sample</h1>
+        </header>
+        <nav>
+          <Link to="/">Home</Link>
+          <span> | </span>
+          <Link to="/about">About Me</Link>
+          <span> | </span>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+        <Switch>
+            <Home exact path="/" component={Home} title=""></Home>
+            <About exact path="/about" component={Home} title=""></About>
+            <Contact exact path="/contact" component={Home} title=""></Contact>
+          </Switch>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
